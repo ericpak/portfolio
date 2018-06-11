27f40020fdeea6f5560d5e7da4a4346ae38cd227
Eric Pak <pak.eric@gmail.com>
2018-06-11 02:09:10 -0700

    pivoted away from pure canvas. Start button works. Social buttons work. Resume pdf works.

--- src/components/desktop.js
+++ src/components/desktop.js
@@ -1,11 +1,4 @@
 import React, { Component } from 'react';
-import Icon from "./icon";
-import Window from "./window";
-
-// Start Bar Images
-import StartButton from "../assets/images/win95_start_button.gif";
-import StartButtonDown from "../assets/images/win95_start_button_down.gif";
-import ClockBg from "../assets/images/clock_bg.gif";
 
 // Icon Images
 import linkedIn_icon from "../assets/images/linkedIn_icon.png";
@@ -15,193 +8,60 @@ import folder_icon from "../assets/images/folder_icon.png";
 import recycle_bin_icon from "../assets/images/recycle_bin_icon.png";
 import pdf_icon from "../assets/images/pdf_icon.png";
 
-var windows;
-var icons;
-
-var mouse_down = false;
+// resume
+import Pdf from "../assets/pdf/resume/Eric_Pak.pdf";
 
 class Desktop extends Component {
   constructor() {
     super();
     this.state = {
-      x: 0,
-      y: 0,
-      width: 0,
-      height: 0,
-      desktopCanvas: undefined,
-      ctx: undefined,
-      startBar: { height: 40 },
-      startButton: { up: true },
       desktopStyle: {
-        height: window.innerHeight - 40
+        height: window.innerHeight - 42
       }
     }
-  }
-
-  componentDidMount() {
-    this.state.desktopCanvas = this.refs.desktopCanvas;
-    this.state.ctx = this.state.desktopCanvas.getContext("2d");
-    this.resizeWindow();
-
-    windows = [];
-    icons = [];
-
-    this.createIcons();
-    this.defaults();
-
-    window.addEventListener('resize', this.resizeWindow.bind(this));
-    this.animate();
-  }
-
-  defaults() {
-    this.startButton();
-    this.clock();
-  }
-
-  _onMouseMove(e) {
-    this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
-    if(mouse_down){
-      console.log(this.state.x + " " + this.state.y);
-    }
-  }
-
-  _onMouseDown(e) {
-    mouse_down = true;
-    // Clicked on Start Button
-    if(this.state.x >= 3 && this.state.x <= 83 && this.state.y >= window.innerHeight-37 && this.state.y <= window.innerHeight - 2)
-      this.setState({ startButton: { up: !this.state.startButton.up } });
-    else {
-      this.setState({ startButton: { up: true } });
-    }
-    // Check if icons were Clicked
-    for(var pos = 0; pos < icons.length; pos++){
-      if(this.state.x >= icons[pos].state.x && this.state.x <= icons[pos].state.x+icons[pos].state.width){
-        if(this.state.y >= icons[pos].state.y && this.state.y <= icons[pos].state.y+icons[pos].state.height){
-          console.log(icons[pos].state.name + " clicked!");
-          if(icons[pos].state.linkText === "folder"){
-            windows.push(new Window(this.state.ctx, icons[pos].state.name, 100, 100, 100, 100));
-            console.log("WINDOW CREATED!");
-          } else {
-            icons[pos].action();
-          }
-        }
-      }
-    }
-  }
-
-  _onMouseUp(e) {
-    mouse_down = false;
-  }
-
-  createIcons(){
-    this.githubExe();
-    this.linkedInExe();
-    this.emailExe();
-    this.deckard_cain();
-    this.snap();
-    this.rilke_schule();
-    this.recycle_bin();
-    this.resume();
-  }
-
-  createIcon(name, x, y, width, height, image, linkText) {
-    icons.push(new Icon(this.state.ctx, name, x, y, width, height, image, linkText));
-  }
-
-  // Contact Icons
-  githubExe() {
-    this.createIcon("GitHub", this.state.desktopCanvas.width-100, this.state.desktopCanvas.height-110, 50, 50, github_icon, "https://github.com/ericpak");
-  }
-  linkedInExe() {
-    this.createIcon("LinkedIn", this.state.desktopCanvas.width-200, this.state.desktopCanvas.height-110, 50, 50, linkedIn_icon, "https://www.linkedin.com/in/eric-pak/");
-  }
-  emailExe() {
-    this.createIcon("Email", this.state.desktopCanvas.width-300, this.state.desktopCanvas.height-110, 50, 50, email_icon, "mailto:pak.eric@gmail.com");
-  }
-
-  // Resume link
-  resume() {
-    this.createIcon("Resume.pdf", this.state.desktopCanvas.width-100, 50, 50, 50, pdf_icon, "/resume");
-  }
-
-  // Folders
-  deckard_cain() {
-    this.createIcon("Deckard Cain", 20, 50, 50, 50, folder_icon, "folder");
-  }
-  snap() {
-    this.createIcon("SNAP", 20, 125, 50, 50, folder_icon, "folder");
-  }
-  rilke_schule() {
-    this.createIcon("Rilke Schule", 20, 200, 50, 50, folder_icon, "folder");
-  }
-  recycle_bin() {
-    this.createIcon("Recycle Bin", 20, 275, 50,50, recycle_bin_icon, "folder");
-  }
-
-  startButton() {
-    let img = new Image();
-    img.src = this.state.startButton.up ? StartButton : StartButtonDown;
-    img.onload = () => {
-      img.height = img.height/2;
-      img.width = img.width/2;
-      this.state.ctx.drawImage(img, 3, window.innerHeight-37, 80, 35);
-    }
-  }
-
-  clock() {
-    let img = new Image();
-    img.src = ClockBg;
-    img.onload = () => {
-      img.height = img.height/2;
-      img.width = img.width/2;
-      this.state.ctx.drawImage(img, window.innerWidth-83, window.innerHeight-37, 80, 35);
-      this.state.ctx.fillStyle = "black";
-      this.state.ctx.font = "18px arial";
-      let d = new Date();
-      let hours = d.getHours();
-      hours = hours%12;
-      if(hours == 0)
-        hours = '12';
-      else if(hours < 10)
-        hours ='0'+hours;
-      let minutes = d.getMinutes();
-      if(minutes < 10)
-        minutes = '0'+minutes;
-      this.state.ctx.fillText(hours+":"+minutes, this.state.desktopCanvas.width-65, this.state.desktopCanvas.height-13);
-    }
-  }
-
-  resizeWindow() {
-    this.state.desktopCanvas.width = window.innerWidth;
-    this.state.desktopCanvas.height = window.innerHeight;
-    this.setState({ desktopStyle: { height: window.innerHeight - 40 }});
-  }
-
-  // Recursive Method
-  animate() {
-    window.requestAnimationFrame(() => {
-      this.animate();
-    });
-    for(var pos = 0; pos < icons.length; pos++){
-      icons[pos].update();
-    }
-    for(pos = 0; pos < windows.length; pos++){
-      windows[pos].update();
-    }
-    this.defaults();
   }
 
   render() {
     return (
       <div className="Desktop" style={this.state.desktopStyle}>
-        <canvas
-          ref="desktopCanvas"
-          id="desktopCanvas"
-          className="desktopCanvas"
-          onMouseMove={ this._onMouseMove.bind(this) }
-          onMouseDown={ this._onMouseDown.bind(this) }
-          onMouseUp={ this._onMouseUp.bind(this) }
-        />
+        <div className="left">
+          <div className="left icon">
+            <input className="folder" id="deckardCain" type="image" src={folder_icon} height="50" width="50" />
+            <p>Deckard Cain</p>
+          </div>
+          <div className="left icon">
+            <input className="folder" id="SNAP" type="image" src={folder_icon} height="50" width="50" />
+            <p>SNAP</p>
+          </div>
+          <div className="left icon">
+            <input className="folder" id="rilkeSchule" type="image" src={folder_icon} height="50" width="50" />
+            <p>Rilke Schule</p>
+          </div>
+          <div className="left icon">
+            <input className="folder" id="recycleBin" type="image" src={recycle_bin_icon} height="50" width="50" />
+            <p>Recycle Bin</p>
+          </div>
+        </div>
+        <div className="rightside">
+          <div className="right icon">
+            <a href={Pdf} target="_blank"><input className="resume" id="resume" type="image" src={pdf_icon} height="50" width="50"/></a>
+            <p>Resume</p>
+          </div>
+          <div className="bottom">
+            <div className="right">
+              <a href="https://github.com/ericpak"><input className="boticon" id="github" type="image" src={github_icon} height="50" width="50" /></a>
+              <p>GitHub</p>
+            </div>
+            <div className="right">
+              <a href="https://www.linkedin.com/in/eric-pak/"><input className="boticon" id="linkedIn" type="image" src={linkedIn_icon} height="50" width="50" /></a>
+              <p>LinkedIn</p>
+            </div>
+            <div className="right">
+              <a href="mailto:pak.eric@gmail.com"><input className="boticon" id="email" type="image" src={email_icon} height="50" width="50" /></a>
+              <p>Email</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/window.js
+++ src/components/window.js
@@ -12,14 +12,34 @@ class Window{
 
   draw() {
     // Basic window
-    this.state.ctx.fillStyle = "black";
-    this.state.ctx.fillRect(this.state.x, this.state.y, this.state.width, this.state.height);
+    this.basicBox(this.state.x, this.state.y, this.state.width, this.state.height, "black", "white");
+
+    // Window Top Bar
+    this.state.ctx.fillStyle = "blue";
+    this.state.ctx.fillRect(this.state.x+4, this.state.y+4, this.state.width-8, 18);
     this.state.ctx.fillStyle = "white";
-    this.state.ctx.fillRect(this.state.x, this.state.y, this.state.width-2, this.state.height-2);
+    this.state.ctx.font = "14px arial";
+    this.state.ctx.fillText(this.state.name, this.state.x+6, this.state.y+18);
+
+    // Window close and minimize
+    this.basicBox(this.state.x+this.state.width-20, this.state.y+6, 14, 14, "black", "white");
+    this.basicBox(this.state.x+this.state.width-40, this.state.y+6, 14, 14, "black", "white");
+    this.state.ctx.fillStyle = "black";
+    this.state.ctx.font = "14px arial";
+    this.state.ctx.fillText("x", this.state.x+this.state.width-18, this.state.y+17);
+    this.state.ctx.fillText("_", this.state.x+this.state.width-38, this.state.y+13);
+
+    // Content Box
+    this.basicBox(this.state.x+4, this.state.y+24, this.state.width-8, this.state.height-28, "white", "black");
+  }
+
+  basicBox(x, y, width, height, color1, color2) {
+    this.state.ctx.fillStyle = color1;
+    this.state.ctx.fillRect(x, y, width, height);
+    this.state.ctx.fillStyle = color2;
+    this.state.ctx.fillRect(x, y, width-2, height-2);
     this.state.ctx.fillStyle = "#bfbfbf";
-    this.state.ctx.fillRect(this.state.x+2, this.state.y+2, this.state.width-4, this.state.height-4);
-    this.state.ctx.font = "14px helvetica";
-    this.state.ctx.fillText(this.state.name, this.state.x, this.state.y + 63);
+    this.state.ctx.fillRect(x+2, y+2, width-4, height-4);
   }
 
   update() {
